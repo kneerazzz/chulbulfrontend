475f95d1ce3363994f3184f430c4ecb3c3c5de95
kneerazzz <troyyig@gmail.com>
2025-08-23 12:06:25 +0530

    final final

--- src/app/components/auth/signupForm.tsx
+++ src/app/components/auth/signupForm.tsx
@@ -29,8 +29,8 @@ const FormSchema = z.object({
   email: z.string().email({
     message: "Please enter a valid email address.",
   }),
-  password: z.string().min(6, {
-    message: "Password must be at least 6 characters.",
+  password: z.string().min(4, {
+    message: "Password must be at least 4 characters.",
   }),
 });
 
